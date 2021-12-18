b0c472a07d838fb3d6f4f1ae93feb10ab8e9a796
jjseh <jvjoseph18@gmail.com>
2021-12-18 12:35:17 +0000

    Github API testing

--- Github Metric Visualisation/GithubVisualisation.py
+++ Github Metric Visualisation/GithubVisualisation.py
@@ -1,3 +1,40 @@
+from github import Github
+from pprint import pprint
+import base64
+
+
+# Github Username
+# Get the URL to request
+# API call for username
+Url = f"https://api.github.com/users/{username}"
+# Request information and return the information as a json
+
+# Pretty print the JSON data/strings
+pprint(collected_data)
+ 
+github_username = input("Enter Github username: ")
+
+
+try:
+    authentication_token = input("Enter OAuth Token: ")
+    accessToken = Github(authentication_token)
+    githubUser = accessToken.get_user(github_username)
+    print("Valid Token : Accepted")
+except:
+    print("invalid")
+    accessToken = Github()
+    githubUser = accessToken.get_user(github_username)
+    
+def repositoriesPrint(repositories):
+     
+     # Print Repository Content 
+     for info in repositories.getInfo(""):
+         print(info)
+     # try catch - error handling
+     try:
+        print("License:", base64.b64decode(repositories.get_license().info.encode()).decode())
+     except:
+       
 
 if __name__ == '__main__':
 
